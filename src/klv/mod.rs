86374a98c0fea7d76a8bf49594e178cdab9c0879
Geordon Worley <vadixidav@gmail.com>
2018-05-28 01:29:40 -0400

    modify name to be shorter

--- src/klv/mod.rs
+++ src/klv/mod.rs
@@ -5,8 +5,9 @@ const UAS_DATALINK_LOCAL_SET_UNIVERSAL_KEY: [u8; 16] = [
     0x06, 0x0E, 0x2B, 0x34, 0x02, 0x0B, 0x01, 0x01, 0x0E, 0x01, 0x03, 0x01, 0x01, 0x00, 0x00, 0x00,
 ];
 
+/// Get the bytes of a UAS Datalink Local Set Packet payload.
 named!(
-    uas_datalink_ls_packet,
+    udl_bytes,
     do_parse!(
         tag!(UAS_DATALINK_LOCAL_SET_UNIVERSAL_KEY) >> length: call!(ber::length)
             >> bytes: take!(length) >> (bytes)
@@ -15,17 +16,17 @@ named!(
 
 #[cfg(test)]
 mod tests {
-    use super::uas_datalink_ls_packet;
+    use super::udl_bytes;
 
     #[test]
-    fn test_uas_datalink_ls_packet_extraction() {
+    fn good_uas_datalink_ls_packet_bytes_extraction() {
         let bytes = [
             0x06, 0x0E, 0x2B, 0x34, 0x02, 0x0B, 0x01, 0x01, 0x0E, 0x01, 0x03, 0x01, 0x01, 0x00,
             0x00, 0x00, 0x04, 0x01, 0x02, 0x03, 0x04,
         ];
 
         assert_eq!(
-            uas_datalink_ls_packet(&bytes[..]),
+            udl_bytes(&bytes[..]),
             Ok((&[] as &[_], &[0x01, 0x02, 0x03, 0x04][..]))
         );
     }
