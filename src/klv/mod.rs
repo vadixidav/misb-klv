19490e690fb2dab1a1454d2467dac80c9f07fb8e
Geordon Worley <vadixidav@gmail.com>
2018-05-28 01:31:29 -0400

    also make universal key name shorter

--- src/klv/mod.rs
+++ src/klv/mod.rs
@@ -1,7 +1,8 @@
 mod ber;
 mod ber_oid;
 
-const UAS_DATALINK_LOCAL_SET_UNIVERSAL_KEY: [u8; 16] = [
+/// The UAS Datalink Local Set Universal Key found at the beginning of a MISB 601 packet.
+const UDL_UNIVERSAL_KEY: [u8; 16] = [
     0x06, 0x0E, 0x2B, 0x34, 0x02, 0x0B, 0x01, 0x01, 0x0E, 0x01, 0x03, 0x01, 0x01, 0x00, 0x00, 0x00,
 ];
 
@@ -9,8 +10,7 @@ const UAS_DATALINK_LOCAL_SET_UNIVERSAL_KEY: [u8; 16] = [
 named!(
     udl_bytes,
     do_parse!(
-        tag!(UAS_DATALINK_LOCAL_SET_UNIVERSAL_KEY) >> length: call!(ber::length)
-            >> bytes: take!(length) >> (bytes)
+        tag!(UDL_UNIVERSAL_KEY) >> length: call!(ber::length) >> bytes: take!(length) >> (bytes)
     )
 );
 
